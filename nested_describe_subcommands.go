196b1f9d05218058bb84e8a6122a3bf7e2c0674c
Naofumi Okamoto <naofumi_okamoto@dwango.co.jp>
2019-05-31 14:50:01 +0900

    Update readme

--- nested_describe_subcommands.go
+++ nested_describe_subcommands.go
@@ -18,19 +18,20 @@ func (*nestedDescribeSubcommands) Synopsis() string {
 }
 
 func (*nestedDescribeSubcommands) Usage() string {
-	return "describe\n\tPrint flags.\n"
+	return "describe <text>\n\tPrint flags.\n"
 }
 
 func (*nestedDescribeSubcommands) SetFlags(_ *flag.FlagSet) {}
 
 func (c *nestedDescribeSubcommands) Execute(ctx context.Context, f *flag.FlagSet, args ...interface{}) subcommands.ExitStatus {
-	if len(f.Args()) != 0 {
+	if len(f.Args()) != 1 {
 		f.Usage()
 		return subcommands.ExitUsageError
 	}
 
 	fmt.Printf("root=%v\n", *root)
 	fmt.Printf("nested=%v\n", *nested)
+	fmt.Printf("text=%s\n", f.Arg(0))
 
 	return subcommands.ExitSuccess
 }
