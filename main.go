2418e2c53507bb16832c73a95f1f5257419fa4ee
Naofumi Okamoto <naofumi_okamoto@dwango.co.jp>
2019-05-31 14:45:31 +0900

    Add nested subcommands

--- main.go
+++ main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"flag"
+	"github.com/google/subcommands"
+	"os"
+)
+
+var root = flag.String("root", "default value", "root flag")
+
+func main() {
+	flag.Parse()
+
+	subcommands.ImportantFlag("root")
+
+	subcommands.Register(subcommands.HelpCommand(), "")
+	subcommands.Register(subcommands.FlagsCommand(), "")
+	subcommands.Register(subcommands.CommandsCommand(), "")
+
+	subcommands.Register(&nestedSubcommands{}, "subcommands")
+
+	ctx := context.Background()
+	os.Exit(int(subcommands.Execute(ctx)))
+}
